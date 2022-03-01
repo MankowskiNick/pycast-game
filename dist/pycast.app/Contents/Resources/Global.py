62cfcdce9b51bd0b4e29cf00bf357f47169ccb64
Nick Mankowski <nnmankowski@owu.edu>
2022-03-01 02:15:00 -0500

    Updated setup.py

--- dist/pycast.app/Contents/Resources/Global.py
+++
@@ -1,9 +0,0 @@
-global level, map1, map2, map3
-global size, width, height
-global screen
-global clock
-global font
-global turnAdjust
-global playerX, playerY, playerAngle
-global fov
-global black, white, gray, red, blue
